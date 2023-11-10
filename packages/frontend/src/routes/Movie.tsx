39fabdff7388865bd1a5df23b28d9fc71ce19b70
changeonPark <qkrcksrjs@gmail.com>
2023-11-10 15:08:45 +0900

    🚩 feat: local only state
    * Fragment

--- packages/frontend/src/routes/Movie.tsx
+++ packages/frontend/src/routes/Movie.tsx
@@ -7,23 +7,48 @@ const GET_MOVIE = gql`
       id
       title
       small_cover_image
+      isLiked @client
     }
   }
 `
 
 const Movie = () => {
   const { id } = useParams()
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache },
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: id,
     },
   })
 
+  const onClick = () => {
+    // https://github.com/apollographql/apollo-cache-persist
+    cache.writeFragment({
+      id: `Movie:${id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          # title
+          isLiked
+        }
+      `,
+      data: {
+        // title: "Sex on the Beach",
+        isLiked: !data.movie.isLiked,
+      },
+    })
+  }
+
   if (loading) return <h1>Loading...</h1>
 
-  console.log(data)
-
-  return <div>{data.movie.title}</div>
+  return (
+    <div>
+      <div>{data.movie.title}</div>
+      <button onClick={onClick}>{data.movie.isLiked ? "Unlike" : "Like"}</button>
+    </div>
+  )
 }
 
 export default Movie
