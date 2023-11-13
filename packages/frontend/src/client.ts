a0849a4a91ec561d47b927c7c4ab6ada61c58fa1
changeonPark <qkrcksrjs@gmail.com>
2023-11-13 15:18:43 +0900

    🚩 add: client schema

--- packages/frontend/src/client.ts
+++ packages/frontend/src/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 // TODO: InmemoryCache가 뭔지 알아보기
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 })
 
